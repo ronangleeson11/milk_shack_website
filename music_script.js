9b028000299f5fa0c6d2e352a4b2a1180db0fb9c
Ronan Gleeson <ronangleeson11@gmail.com>
2026-01-12 01:39:37 -0500

    Add pointer to currently playing song

--- music_script.js
+++ music_script.js
@@ -1,12 +1,17 @@
-let music_player = document.querySelector("#music-player");
-let tracklist = document.querySelector("#tracklist");
+const music_player = document.querySelector("#music-player");
+const tracklist = document.querySelector("#tracklist");
 tracklist.addEventListener("click", (event) => {
-        const li = event.target.closest('li');
-        if (!li || !tracklist.contains(li)) return;
-        const song = li.id;
-        if (!song) return;
-        const src = `music/${song}`;
-        music_player.src = src;
-        music_player.load();
-        music_player.play().catch(() => {});
-    });
+    const li = event.target.closest('li');
+    if (!li || !tracklist.contains(li)) return;
+    const selected_song = li.id;
+    if (!selected_song) return;
+    let selected_songs = document.querySelectorAll(".selected-song");
+    for (song of selected_songs) {
+        song.classList.remove("selected-song");
+    }
+    li.classList.add("selected-song");
+    const src = `music/${selected_song}`;
+    music_player.src = src;
+    music_player.load();
+    music_player.play().catch(() => {});
+});
